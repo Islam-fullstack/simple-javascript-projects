7e5d1edfa2ed99a9fd02be524af3eb3ebde9737c
islam-fullstack <islam.qiyasov0823@gmail.com>
2021-07-31 18:43:31 +0500

    1st project finished

--- Project 2 Counter/script.js
+++ Project 2 Counter/script.js
@@ -1,3 +1,19 @@
 window.addEventListener('DOMContentLoaded', function () {
-    console.log('Hello World')
+    const displayNumber = document.querySelector('.number');
+    const btnDecrease = document.querySelector('.decrease');
+    const btnReset = document.querySelector('.reset');
+    const btnIncrease = document.querySelector('.increase')
+    btnDecrease.addEventListener('click', () => {
+        let currentNumber = parseInt(displayNumber.textContent);
+        displayNumber.textContent = (--currentNumber).toString();
+        console.log(currentNumber);
+    })
+
+    btnReset.addEventListener('click', () => {
+        displayNumber.textContent = '0';
+    })
+
+    btnIncrease.addEventListener('click', () => {
+        displayNumber.textContent = (++displayNumber.textContent);
+    })
 })
